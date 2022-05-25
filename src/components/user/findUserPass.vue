9faee7c7570bd5d12424cb77cffcb76c3e48583b
choijihye <chlwlgp11@naver.com>
2022-05-25 19:41:22 +0900

    user

--- src/components/user/findUserPass.vue
+++ src/components/user/findUserPass.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h5 class="font-semibold text-center">비밀번호 찾기</h5>
+    <p class="text-center">새로운 비밀번호로 변경하십시오.</p>
     <a-form
       id="components-form-demo-normal-login"
       :label-col="{ span: 5 }"
@@ -23,7 +24,7 @@
           <a-form-item>
             <a-input addon-before="새 비밀번호 확인 " v-model="userpasscheck" />
             <span class="ml-30 text-danger" v-if="!newpassinput"
-              >이름을 입력하여 주세요.</span
+              >비밀번호가 일치하지 않습니다.</span
             >
           </a-form-item>
         </a-form>
@@ -60,6 +61,8 @@
 
 <script>
 import http from "@/api/http";
+import { mapState, mapMutations } from "vuex";
+const memberStore = "memberStore";
 export default {
   data() {
     return {
@@ -78,12 +81,38 @@ export default {
     // Creates the form and adds to it component's "form" property.
     this.form = this.$form.createForm(this, { name: "normal_login" });
   },
+  computed: {
+    ...mapState(memberStore, ["userInfo"]),
+    ...mapMutations(memberStore, ["changeUser"]),
+  },
   methods: {
     changepass() {
       this.passinput = true;
       this.newpassinput = true;
       if (!this.userpass) {
         this.passinput = false;
+      }
+      if (this.userpass !== this.userpasscheck) {
+        this.newpassinput = false;
+      } else if (this.passinput && this.newpassinput) {
+        // 비밀번호 변경 시켜야 해
+        let now = this;
+        http
+          .put("/restuser/changepass", null, {
+            params: {
+              userid: this.userid,
+              username: this.username,
+              userpass: this.userpass,
+            },
+          })
+          .then(({ data }) => {
+            if (data === "success") {
+              alert("새로운 비밀번호로 변경되었습니다.");
+            } else {
+              alert("비밀번호 변경에 실패하였습니다.");
+            }
+            now.visible = false;
+          });
       }
     },
     handleCancel() {
@@ -98,17 +127,18 @@ export default {
       if (!this.userid) {
         this.idinput = false;
       }
+
       let userid = this.userid;
       let username = this.username;
-      let result = "";
+      let isuser = this;
       http
         .get("/restuser/findpass/" + userid + "/" + username)
         .then(function (response) {
-          result = response.data;
           if (response.data === "") {
             alert("회원정보가 존재하지 않습니다");
             return;
           } else {
+            isuser.visible = true;
           }
         });
     },
